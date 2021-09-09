8d927ded62b0ffa4435b8648e4cefdffb63f2e35
Alexandre Morey <alexander006_9@hotmail.com>
2021-09-09 10:33:54 +0200

    page for game whack

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,4 +7,7 @@ class PagesController < ApplicationController
 
   def contact
   end
+
+  def game_whack_the_alex
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'game_whack_the_alex', to: 'pages#game_whack_the_alex'
+
 end
